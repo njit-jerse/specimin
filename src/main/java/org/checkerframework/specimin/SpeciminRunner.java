f51c13fec387d0557004eed3033d500d1298eeae
Martin Kellogg <kelloggm@cs.washington.edu>
2021-12-16 16:48:38 -0800

    code formatting, etc

---
+++ src/main/java/org/checkerframework/specimin/SpeciminRunner.java
@@ -0,0 +1,12 @@
+package org.checkerframework.specimin;
+
+/** This class is the main runner for Specimin. Use its main() method to start Specimin. */
+public class SpeciminRunner {
+
+  /**
+   * The main entry point for Specimin.
+   *
+   * @param args the arguments to Specimin
+   */
+  public static void main(String... args) {}
+}
