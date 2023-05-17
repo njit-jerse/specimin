af3f48b8fa27ccb4b60a27c97a63fe6bb3f30d28
Loi Nguyen <113363230+LoiNguyenCS@users.noreply.github.com>
2023-05-17 15:27:03 -0400

    delete call-existing methods

--- src/test/resources/callexistingmethod/input/com/example/Simple.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-import java.util.Random;
-
-public class Simple {
-    public void test() {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-    }
-}
