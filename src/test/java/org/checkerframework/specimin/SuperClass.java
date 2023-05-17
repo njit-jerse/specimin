bf29ad268c4798d1ce04ace010b82d088107864f
LoiNguyenCS <ln3@njit.edu>
2023-05-17 13:50:14 -0400

    clarify comments for superclass

--- src/test/java/org/checkerframework/specimin/SuperClass.java
+++ src/test/java/org/checkerframework/specimin/SuperClass.java
@@ -4,8 +4,9 @@ import java.io.IOException;
 import org.junit.Test;
 
 /**
- This test verifies that when a Java file contains a targeted method that overrides a method in a superclass,
- the minimized version of the Java file will include the original method from the superclass with an empty body.
+ * This test verifies that when a Java file contains a targeted method that overrides a method in a
+ * superclass, the minimized version of the Java file will include the original method from the
+ * superclass with an empty body.
  */
 public class SuperClass {
   @Test
