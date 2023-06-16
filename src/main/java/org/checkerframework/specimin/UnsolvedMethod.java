b2e9143d6949e8b81d94dd561617260e0fa8b2be
LoiNguyenCS <ln3@njit.edu>
2023-06-16 15:19:42 -0400

    add parameter list for methods

--- src/main/java/org/checkerframework/specimin/UnsolvedMethod.java
+++ src/main/java/org/checkerframework/specimin/UnsolvedMethod.java
@@ -1,5 +1,8 @@
 package org.checkerframework.specimin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * An UnsolvedMethod instance is a representation of a method that can not be solved by
  * SymbolSolver. The reason is that the class file of that method is not in the root directory.
@@ -15,6 +18,12 @@ public class UnsolvedMethod {
   private final String returnType;
 
   /**
+   * The list of parameters of the method. (Right now we won't touch it until the new variant of
+   * SymbolSolver is available)
+   */
+  private List<String> parameterList;
+
+  /**
    * Create an instance of UnsolvedMethod
    *
    * @param name the name of the method
@@ -23,8 +32,12 @@ public class UnsolvedMethod {
   public UnsolvedMethod(String name, String returnType) {
     this.name = name;
     this.returnType = returnType;
+    this.parameterList = new ArrayList<>();
   }
 
+  public List<String> getParameterList() {
+    return parameterList;
+  }
   /**
    * Return the content of the method. Note that the body of the method is stubbed out.
    *
