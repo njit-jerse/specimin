8676d4b138c84a5bf3df31bf0f3dd778d6a87b13
unknown <ln3@njit.edu>
2023-06-24 21:21:49 -0400

    replace wildcards import statements

--- src/main/java/org/checkerframework/specimin/UnsolvedSymbolVisitor.java
+++ src/main/java/org/checkerframework/specimin/UnsolvedSymbolVisitor.java
@@ -15,7 +15,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 
 /**
  * The visitor for the preliminary phase of Specimin. This visitor goes through the input files,
