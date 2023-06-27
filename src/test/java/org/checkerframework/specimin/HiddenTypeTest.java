77cd2862a6ad04278fafc7427a8b432270067d15
unknown <ln3@njit.edu>
2023-06-27 19:49:07 -0400

    add javadoc for test

--- src/test/java/org/checkerframework/specimin/HiddenTypeTest.java
+++ src/test/java/org/checkerframework/specimin/HiddenTypeTest.java
@@ -4,15 +4,16 @@ import java.io.IOException;
 import org.junit.Test;
 
 /**
- * This test checks that if the targetted method in a Java file uses a global variable, the global
- * variable will be included in the minimized version of the Java file.
+ * This test checks that if the targetted method in a Java file is called by an unsolved method,
+ * meaning that the source file of the caller method is not in the root directory, Specimin can
+ * create appropriate synthetic files so that the output is compilable
  */
 public class HiddenTypeTest {
   @Test
   public void runTest() throws IOException {
     SpeciminTestExecutor.runTest(
-            "hiddenType",
-            new String[] {"com/example/Simple.java"},
-            new String[] {"com.example.Simple#isVoidType(MethodDeclaration)"});
+        "hiddenType",
+        new String[] {"com/example/Simple.java"},
+        new String[] {"com.example.Simple#isVoidType(MethodDeclaration)"});
   }
 }
