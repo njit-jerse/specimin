a7450242120eb62a975e6ae3060668f57323b271
unknown <ln3@njit.edu>
2023-11-14 12:49:26 -0500

    a proper String value

--- src/test/resources/unsolvedstaticmethod/expected/com/example/Simple.java
+++ src/test/resources/unsolvedstaticmethod/expected/com/example/Simple.java
@@ -7,7 +7,7 @@ class Simple {
 
     void bar() {
         int x = 5;
-        String y = 7;
+        String y = "hello";
         AClass z = new AClass();
         MyClass.process(x);
         org.testing.ThisClass.process(y, z);
