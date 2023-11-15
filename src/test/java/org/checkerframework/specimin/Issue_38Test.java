eeec7edf3876a4595ea86f89ca445eb1e4f71d36
Tahiatul Islam <tahiatul@tahiatuls-mbp.myfiosgateway.com>
2023-11-15 03:04:07 -0500

    test runner add

---
+++ src/test/java/org/checkerframework/specimin/Issue_38Test.java
@@ -0,0 +1,17 @@
+ackage org.checkerframework.specimin;
+
+import java.io.IOException;
+import org.junit.Test;
+
+/**
+ * This test checks if Specimin will work for array types
+ */
+public class Issue_38Test {
+  @Test
+  public void runTest() throws IOException {
+    SpeciminTestExecutor.runTestWithoutJarPaths(
+        "unionType",
+        new String[] {"com/example/Issue_38.java"},
+        new String[] {"com.example.Issue_38#test()"});
+  }
+}
