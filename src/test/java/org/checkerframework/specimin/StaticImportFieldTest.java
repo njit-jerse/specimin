46f8293c89e4146be99f65564869f3bbfeaa2ac8
unknown <ln3@njit.edu>
2023-12-07 15:52:05 -0500

    fix test files

--- src/test/java/org/checkerframework/specimin/StaticImportFieldTest.java
+++ src/test/java/org/checkerframework/specimin/StaticImportFieldTest.java
@@ -1,19 +1,15 @@
-
 package org.checkerframework.specimin;
 
+import java.io.IOException;
 import org.junit.Test;
 
-import java.io.IOException;
-
-/**
- * This test checks if Specimin can handle an unsolved and statically imported field.
- */
+/** This test checks if Specimin can handle an unsolved and statically imported field. */
 public class StaticImportFieldTest {
   @Test
   public void runTest() throws IOException {
     SpeciminTestExecutor.runTestWithoutJarPaths(
         "staticimportfield",
         new String[] {"com/example/Simple.java"},
-        new String[] {"com.example.Simple#bar()"});
+        new String[] {"com.example.Simple#test()"});
   }
 }
--- src/test/java/org/checkerframework/specimin/StaticImportMethodTest.java
+++ src/test/java/org/checkerframework/specimin/StaticImportMethodTest.java
@@ -1,19 +1,15 @@
-
 package org.checkerframework.specimin;
 
+import java.io.IOException;
 import org.junit.Test;
 
-import java.io.IOException;
-
-/**
- * This test checks if Specimin can handle an unsolved and statically imported method.
- */
+/** This test checks if Specimin can handle an unsolved and statically imported method. */
 public class StaticImportMethodTest {
   @Test
   public void runTest() throws IOException {
     SpeciminTestExecutor.runTestWithoutJarPaths(
         "staticimportmethod",
         new String[] {"com/example/Simple.java"},
-        new String[] {"com.example.Simple#bar()"});
+        new String[] {"com.example.Simple#test()"});
   }
 }
