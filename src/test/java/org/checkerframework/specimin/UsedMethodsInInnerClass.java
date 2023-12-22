4f2189f12115487a2f190b317b6291df7158d634
unknown <ln3@njit.edu>
2023-12-22 18:09:51 -0500

    fix broken test file

--- src/test/java/org/checkerframework/specimin/UsedMethodsInInnerClass.java
+++ src/test/java/org/checkerframework/specimin/UsedMethodsInInnerClass.java
@@ -1,11 +1,11 @@
 package org.checkerframework.specimin;
 
+import java.io.IOException;
 import org.junit.Test;
 
-import java.io.IOException;
-
 /**
- * This test checks if Specimin can preserve inner classes that contain used methods and remove inner classes that do not.
+ * This test checks if Specimin can preserve inner classes that contain used methods and remove
+ * inner classes that do not.
  */
 public class UsedMethodsInInnerClass {
   @Test
