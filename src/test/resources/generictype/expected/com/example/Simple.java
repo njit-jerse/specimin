a407f2ee2fbae9b195998f3e572dc845967a74eb
unknown <ln3@njit.edu>
2024-01-23 14:55:02 -0500

    add codes and tests

---
+++ src/test/resources/generictype/expected/com/example/Simple.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Simple<T> {
+
+    public Simple(T content) {
+        this.content = content;
+    }
+}
---
+++ src/test/resources/generictype/input/com/example/Simple.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import com.sun.source.tree.BinaryTree;
+import org.test.BinaryTree;
+import java.util.Map;
+
+public class Simple<T> {
+    private Map<BinaryTree, Integer> tree;
+
+    public Simple(T content) {
+        this.content = content;
+    }
+}
+
