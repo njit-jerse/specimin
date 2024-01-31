4e64dd59d1533b863bbf95f08142aa0717a90411
unknown <ln3@njit.edu>
2024-01-31 11:35:11 -0500

    enforce not-touching-original-files rule

--- src/main/java/org/checkerframework/specimin/UnsolvedSymbolVisitor.java
+++ src/main/java/org/checkerframework/specimin/UnsolvedSymbolVisitor.java
@@ -1467,6 +1467,10 @@ public class UnsolvedSymbolVisitor extends ModifierVisitor<Void> {
    * @param missedClass the class to be updated
    */
   public void updateMissingClass(UnsolvedClass missedClass) {
+    if (classfileIsInOriginalCodebase(
+            missedClass.getPackageName() + "." + missedClass.getClassName())) {
+      return;
+    }
     Iterator<UnsolvedClass> iterator = missingClass.iterator();
     while (iterator.hasNext()) {
       UnsolvedClass e = iterator.next();
@@ -1490,6 +1494,24 @@ public class UnsolvedSymbolVisitor extends ModifierVisitor<Void> {
       }
     }
     missingClass.add(missedClass);
+  }
+
+  /**
+   * Given the qualified name of a class, this method determines if the corresponding class file
+   * exists in the original input codebase.
+   *
+   * @param qualifiedName the qualified name of a class.
+   * @return true if the corresponding class file is origi nally in the input codebase.
+   */
+  public boolean classfileIsInOriginalCodebase(String qualifiedName) {
+    String relativeClassPath = qualifiedName.replace(".", "/");
+    int indexOfTypeVariables = relativeClassPath.indexOf("<");
+    if (indexOfTypeVariables != -1) {
+      relativeClassPath = relativeClassPath.substring(0, indexOfTypeVariables);
+    }
+    relativeClassPath = relativeClassPath + ".java";
+    return this.setOfExistingFiles.contains(
+            Paths.get(this.rootDirectory + "/" + relativeClassPath).toAbsolutePath());
   }
 
   /**
