1181390e8d2378a3700208afd3bcc7d3c4a6c952
unknown <ln3@njit.edu>
2024-02-03 20:16:38 -0500

    remove comments from test files

--- src/test/resources/interfaceimplemented/expected/com/example/Baz.java
+++ src/test/resources/interfaceimplemented/expected/com/example/Baz.java
@@ -1,6 +1,5 @@
 package com.example;
 
-// Baz.java
 public interface Baz {
 
     void doSomething();
--- src/test/resources/interfacewithgenerictype/expected/com/example/Baz.java
+++ src/test/resources/interfacewithgenerictype/expected/com/example/Baz.java
@@ -1,10 +1,8 @@
 package com.example;
 
-// Baz.java
 public interface Baz<T> {
 
     void doSomething(T value);
 
-    // sadly we can't remove this method.
     void doSomething(int x);
 }
