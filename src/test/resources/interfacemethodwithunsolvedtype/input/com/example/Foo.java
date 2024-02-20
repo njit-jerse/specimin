17971ea960befcd2e15c9e0b9ab8e552cd048b64
Martin Kellogg <martin.kellogg@njit.edu>
2024-02-20 16:06:18 -0500

    fix another bad test

--- src/test/resources/interfacemethodwithunsolvedtype/input/com/example/Foo.java
+++ src/test/resources/interfacemethodwithunsolvedtype/input/com/example/Foo.java
@@ -6,6 +6,7 @@ class Foo implements Baz<String> {
     @Override
     public UnsolvedType doSomething(String value) {
         System.out.println("Foo is doing something with: " + value);
+        return null;
     }
 
 }
