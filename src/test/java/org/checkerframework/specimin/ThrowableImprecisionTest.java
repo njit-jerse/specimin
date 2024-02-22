e6eb0fc609be1e2da4842ab56e02bf9da538deb2
Martin Kellogg <martin.kellogg@njit.edu>
2024-02-22 12:35:32 -0500

    Explain imprecision

--- src/test/java/org/checkerframework/specimin/ThrowableImprecisionTest.java
+++ src/test/java/org/checkerframework/specimin/ThrowableImprecisionTest.java
@@ -7,7 +7,9 @@ import org.junit.Test;
  * This test demonstrates the imprecision of Specimin when there are two or more synthetic classes
  * with the same name. If only one or some of them extend Throwable, Specimin will imprecisely make
  * all of them extend Throwable. This occurs due to Specimin becoming confused when faced with
- * multiple synthetic classes sharing the same name.
+ * multiple synthetic classes sharing the same name. This imprecision is caused by the fact that
+ * Specimin uses javac's error messages to determine which synthetic types need to extend Throwable,
+ * but javac only prints simple names in its error messages.
  */
 public class ThrowableImprecisionTest {
   @Test
