abfd8e577b122ce156f3dfc2f9533a92c3865d6e
unknown <ln3@njit.edu>
2024-03-08 20:23:17 -0500

    add codes and tests

---
+++ src/test/java/org/checkerframework/specimin/NonPrimaryClassTest.java
@@ -0,0 +1,15 @@
+package org.checkerframework.specimin;
+
+import java.io.IOException;
+import org.junit.Test;
+
+/** This test checks if Specimin can handle target methods in non-primary classes. */
+public class NonPrimaryClassTest {
+  @Test
+  public void runTest() throws IOException {
+    SpeciminTestExecutor.runTestWithoutJarPaths(
+        "nonprimaryclass",
+        new String[] {"com/example/Foo.java"},
+        new String[] {"com.example.Foo#bar()"});
+  }
+}
