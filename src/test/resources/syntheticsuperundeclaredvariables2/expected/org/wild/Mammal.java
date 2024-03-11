3f4a9d864ce66f854b3e4c4286f992638d84d0af
Martin Kellogg <martin.kellogg@njit.edu>
2024-03-11 13:02:13 -0400

    add test case that doesn't work with the new scheme

---
+++ src/test/resources/syntheticsuperundeclaredvariables2/expected/org/wild/Mammal.java
@@ -0,0 +1,6 @@
+package org.wild;
+public class Mammal {
+    public boolean bornFromEggs;
+
+    public int x;
+}
---
+++ src/test/resources/syntheticsuperundeclaredvariables2/input/com/example/Dog.java
@@ -0,0 +1,8 @@
+package com.example;
+import org.wild.Mammal;
+
+public class Dog extends Mammal {
+    public boolean isBornFromEggs() {
+        return x > 10 ? bornFromEggs : false;
+    }
+}
