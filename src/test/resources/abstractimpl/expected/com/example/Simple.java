06d1f05ccce32fd41ab07b5c4eb8914219b36b3a
Martin Kellogg <martin.kellogg@njit.edu>
2024-03-13 14:35:11 -0400

    oops

--- src/test/resources/abstractimpl/expected/com/example/Simple.java
+++ src/test/resources/abstractimpl/expected/com/example/Simple.java
@@ -6,7 +6,6 @@ import java.util.Collection;
 import com.example.WrappedSet;
 
 public class Simple<K, V> {
-    // Target method.
     Collection<V> bar(K key, Collection<V> collection) {
         return new WrappedSet(key, (Set<V>) collection);
     }
