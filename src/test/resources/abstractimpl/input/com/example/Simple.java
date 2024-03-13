b9734f76797241f936bc1b26f25d7295a90ba7ed
Martin Kellogg <martin.kellogg@njit.edu>
2024-03-13 14:34:13 -0400

    fix typo

--- src/test/resources/abstractimpl/input/com/example/Simple.java
+++ src/test/resources/abstractimpl/input/com/example/Simple.java
@@ -7,7 +7,7 @@ import com.example.WrappedSet;
 
 public class Simple<K, V> {
     // Target method.
-    Collection<V> bar(K Key, Collection<V> collection) {
+    Collection<V> bar(K key, Collection<V> collection) {
         return new WrappedSet(key, (Set<V>) collection);
     }
 }
