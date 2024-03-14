fe12ecbb97ca3569480286135ca16ac642392546
Martin Kellogg <martin.kellogg@njit.edu>
2024-03-14 17:48:01 -0400

    fix the tesT

--- src/test/resources/anonclassscope/expected/com/example/Simple.java
+++ src/test/resources/anonclassscope/expected/com/example/Simple.java
@@ -5,6 +5,10 @@ public final class Simple {
     private static Object test() {
         return new Object() {
 
+            public void anotherMethod() {
+
+            }
+
             public String toString() {
                 cast(new Object());
                 return super.toString();
