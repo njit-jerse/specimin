dca2c4fd62cbd20a8ca7131c41eb1260f787fa49
unknown <ln3@njit.edu>
2024-03-13 22:31:11 -0400

    update expected test results

--- src/test/resources/multipleboundtypeparameter/expected/com/example/Simple.java
+++ src/test/resources/multipleboundtypeparameter/expected/com/example/Simple.java
@@ -1,8 +1,10 @@
 package com.example;
 
 import org.testing.UnsolvedType;
+import org.testing.UnsolvedType2;
+import org.testing.UnsolvedType3;
 
-class Simple<I extends UnsolvedType> {
+class Simple<I extends UnsolvedType & UnsolvedType2 & UnsolvedType3> {
 
     I field;
 
