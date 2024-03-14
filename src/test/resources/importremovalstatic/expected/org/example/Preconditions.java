8dcaf23360a84bf9fe8e65d2c7039bb84212990d
Martin Kellogg <martin.kellogg@njit.edu>
2024-03-14 15:23:42 -0400

    failing test

---
+++ src/test/resources/importremovalstatic/expected/org/example/Preconditions.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Preconditions {
+
+    static void checkArgument(Object o) {
+        throw new Error();
+    }
+}
---
+++ src/test/resources/importremovalstatic/input/org/example/Preconditions.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Preconditions {
+    static void checkState(Object o) {
+
+    }
+
+    static void checkArgument(Object o) {
+
+    }
+}
