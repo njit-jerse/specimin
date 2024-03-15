9568e2ea1aa02ec21ee1660a53fb99923f9c546f
Martin Kellogg <martin.kellogg@njit.edu>
2024-03-15 11:33:00 -0400

    rename

--- src/test/java/org/checkerframework/specimin/MethodRefTest.java
+++ src/test/java/org/checkerframework/specimin/MethodRefTest.java
@@ -4,14 +4,14 @@ import java.io.IOException;
 import org.junit.Test;
 
 /**
- * This test checks that Specimin doesn't crash when encountering a used method that takes a
- * function as a parameter.
+ * This test checks that Specimin correctly preserves the signatures of
+ * methods used as methodrefs. Based on an example from Apache Calcite.
  */
-public class FunctionMethodTest {
+public class MethodRefTest {
   @Test
   public void runTest() throws IOException {
     SpeciminTestExecutor.runTestWithoutJarPaths(
-        "functionmethod",
+            "methodref",
         new String[] {"com/example/Simple.java"},
         new String[] {"com.example.Simple#bar(List<SqlNode>)"});
   }
--- src/test/resources/methodref/expected/com/example/Simple.java
+++ src/test/resources/methodref/expected/com/example/Simple.java
@@ -3,14 +3,11 @@ package com.example;
 import java.util.List;
 
 class Simple {
-    // Target method.
+
     public List<Integer> bar(List<SqlNode> nodes) {
         return transform(nodes, SqlNode::getParserPosition);
     }
 
-
-    // From Apache Calcite
-    /** Transforms a list, applying a function to each element. */
     public static <F, T> List<T> transform(List<? extends F> list,
                                            java.util.function.Function<? super F, ? extends T> function) {
         throw new Error();
