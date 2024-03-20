f814513763803127f7e6a4bb42200f876666cb08
Martin Kellogg <martin.kellogg@njit.edu>
2024-03-20 08:35:23 -0400

    failing test

---
+++ src/test/resources/nullarg/expected/com/example/Simple.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import org.example.Baz;
+
+class Simple {
+    void bar(Baz baz) {
+        baz.qux(null);
+    }
+}
---
+++ src/test/resources/nullarg/expected/org/example/Baz.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Baz {
+
+    QuxReturnType qux(Object parameter0) {
+        throw new Error();
+    }
+}
