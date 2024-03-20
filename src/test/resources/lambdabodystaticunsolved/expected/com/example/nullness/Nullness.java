424493fe2897f264bfeb15eadb4a30c50aaf26ba
Martin Kellogg <martin.kellogg@njit.edu>
2024-03-20 15:42:21 -0400

    forgot that I changed the top type from Object to java.lang.Object

--- src/test/resources/lambdabodystaticunsolved/expected/com/example/nullness/Nullness.java
+++ src/test/resources/lambdabodystaticunsolved/expected/com/example/nullness/Nullness.java
@@ -2,7 +2,7 @@ package com.example.nullness;
 
 public class Nullness {
 
-    public static com.example.sql.SqlParserPos castNonNull(Object parameter0) {
+    public static com.example.sql.SqlParserPos castNonNull(java.lang.Object parameter0) {
         throw new Error();
     }
 }
--- src/test/resources/lambdabodystaticunsolved2/expected/com/example/nullness/Nullness.java
+++ src/test/resources/lambdabodystaticunsolved2/expected/com/example/nullness/Nullness.java
@@ -2,7 +2,7 @@ package com.example.nullness;
 
 public class Nullness {
 
-    public static <SyntheticUnconstrainedType> SyntheticUnconstrainedType castNonNull(Object parameter0) {
+    public static <SyntheticUnconstrainedType> SyntheticUnconstrainedType castNonNull(java.lang.Object parameter0) {
         throw new Error();
     }
 }
