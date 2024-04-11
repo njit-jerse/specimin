e52f292f9c54d97648ffd2fba00137822e54994d
Martin Kellogg <martin.kellogg@njit.edu>
2024-04-10 20:00:54 -0400

    test case that demos the problem

---
+++ src/test/resources/navigablesetexample2/expected/com/example/Simple.java
@@ -0,0 +1,27 @@
+package com.example;
+
+/*
+ * Based on an example from java.util.Collections.
+ */
+public class Simple {
+
+    static class UnmodifiableCollection<E> {
+    }
+
+    static class UnmodifiableSet<E> extends UnmodifiableCollection<E> {
+    }
+
+    static class UnmodifiableSortedSet<E> extends UnmodifiableSet<E> {
+    }
+
+    public static <T> NavigableSet<T> unmodifiableNavigableSet(NavigableSet<T> s) {
+        return new UnmodifiableNavigableSet<>(s);
+    }
+
+    static class UnmodifiableNavigableSet<E> extends UnmodifiableSortedSet<E> implements NavigableSet<E> {
+
+        UnmodifiableNavigableSet(NavigableSet<E> s) {
+            throw new Error();
+        }
+    }
+}
