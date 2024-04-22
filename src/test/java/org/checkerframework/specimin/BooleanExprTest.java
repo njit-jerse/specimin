85b30f57c9ba25d33a7328a6b41ea0436864046d
Martin Kellogg <martin.kellogg@njit.edu>
2024-04-22 15:15:10 -0400

    test that properly fails

--- src/test/java/org/checkerframework/specimin/BooleanExprTest.java
+++ src/test/java/org/checkerframework/specimin/BooleanExprTest.java
@@ -1,18 +1,15 @@
 package org.checkerframework.specimin;
 
+import java.io.IOException;
 import org.junit.Test;
 
-import java.io.IOException;
-
-/**
- * This test checks that arguments to boolean expressions always have the type "boolean".
- */
+/** This test checks that arguments to boolean expressions always have the type "boolean". */
 public class BooleanExprTest {
   @Test
   public void runTest() throws IOException {
     SpeciminTestExecutor.runTestWithoutJarPaths(
         "booleanexpr",
         new String[] {"com/example/Simple.java"},
-        new String[] {"com.example.Simple#bar()"});
+        new String[] {"com.example.Simple#bar(Foo)"});
   }
 }
