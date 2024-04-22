ff120fb19cbb8db88e71a04e6d1bf20e35e617b5
Martin Kellogg <martin.kellogg@njit.edu>
2024-04-22 15:00:11 -0400

    rename test

--- src/test/java/org/checkerframework/specimin/CtorArgumentTest.java
+++ src/test/java/org/checkerframework/specimin/CtorArgumentTest.java
@@ -3,12 +3,13 @@ package org.checkerframework.specimin;
 import java.io.IOException;
 import org.junit.Test;
 
-/** This test checks TODO */
-public class CtorInCatchTest {
+/** This test checks that UnsolvedSymbolVisitor doesn't skip unsolved symbols in
+ * the arguments to constructors. */
+public class CtorArgumentTest {
   @Test
   public void runTest() throws IOException {
     SpeciminTestExecutor.runTestWithoutJarPaths(
-        "ctorincatch",
+        "ctorargument",
         new String[] {"com/example/Simple.java"},
         new String[] {"com.example.Simple#bar(MethodGen)"});
   }
