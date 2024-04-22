aa22f07d525d37439f9bc5d639ec7e3bb2bdf7fb
Martin Kellogg <martin.kellogg@njit.edu>
2024-04-22 18:33:03 -0400

    more tests

--- src/test/resources/booleanexpr/expected/com/example/Foo.java
+++ src/test/resources/booleanexpr/expected/com/example/Foo.java
@@ -2,6 +2,8 @@ package com.example;
 
 public class Foo {
 
+    public boolean p;
+
     public boolean isBaz;
 
     public boolean isBar() {
@@ -9,6 +11,22 @@ public class Foo {
     }
 
     public int qux() {
+        throw new Error();
+    }
+
+    public double razz() {
+        throw new Error();
+    }
+
+    public int getX() {
+        throw new Error();
+    }
+
+    public long getLong() {
+        throw new Error();
+    }
+
+    public Long getBigLong() {
         throw new Error();
     }
 }
--- src/test/resources/booleanexpr/input/com/example/Simple.java
+++ src/test/resources/booleanexpr/input/com/example/Simple.java
@@ -3,13 +3,27 @@ package com.example;
 class Simple {
 
     int bar(Foo f) {
-        if (f.isBar() || f.isBaz) {
+        if (f.isBar() || f.isBaz && f.p) {
             return 0;
         }
 
         if (f.qux() == 5) {
             return 1;
         }
+
+        if (f.razz() != 5.4) {
+            return 2;
+        }
+
+        int y = f.getX() + 2;
+        long z = f.getLong() * 3L;
+        Long w = Long.valueOf(100L);
+        Long x = f.getBigLong() / w;
+
+        // Note: this test wouldn't work, because javac doesn't issue
+        // an error about string concatenation with non-strings (everything
+        // has a toString() method!).
+        // String s = f.getS() + "!";
 
         return 2;
     }
