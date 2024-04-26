822a7fc996dfcca8c4af15d9451415a361600da8
Martin Kellogg <martin.kellogg@njit.edu>
2024-04-26 13:31:08 -0400

    failing test

---
+++ src/test/resources/staticandnonstaticuse/expected/org/example/Foo.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Foo {
+
+    public Foo() {
+        throw new Error();
+    }
+
+    public static OrgExampleFooSetThisReturnType setThis(com.example.Simple parameter0) {
+        throw new Error();
+    }
+}
