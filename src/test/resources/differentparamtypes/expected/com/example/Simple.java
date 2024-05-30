c7c83af5f746bb451b512d9fc9f0eea64fc9741c
Martin Kellogg <martin.kellogg@njit.edu>
2024-05-30 09:09:48 -0400

    add test case

---
+++ src/test/resources/differentparamtypes/expected/com/example/Simple.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Simple {
+    void bar() {
+        foo(new ArrayList<?>());
+    }
+
+    void foo(List<?> baz) {
+        throw new Error();
+    }
+}
