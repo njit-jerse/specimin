f535fc2684d997d307dd2db3fe51ce60bad84737
Martin Kellogg <martin.kellogg@njit.edu>
2024-06-19 17:37:33 -0400

    another test which still fails, which is an even more complex version that more closely models the still-failing real example

--- src/test/java/org/checkerframework/specimin/OtherPackageArray2Test.java
+++ src/test/java/org/checkerframework/specimin/OtherPackageArray2Test.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 import org.junit.Test;
 
 /**
- * This test checks for the actual crash in https://github.com/njit-jerse/specimin/issues/86. The
- * difference between this and {@link OtherPackageArrayTest} is that in this version, the called
- * method is a constructor (which was required for the actual crash reported in the issue).
+ * This test checks for a crash related to the one in
+ * https://github.com/njit-jerse/specimin/issues/86. The difference between this and {@link
+ * OtherPackageArrayTest} is that in this version, the called method is a constructor (which was
+ * required for the actual crash reported in the issue). The actual crash is more complex, because
+ * it involves the Type class being in a different package; see {@link OtherPackageArray3Test}
  */
 public class OtherPackageArray2Test {
   @Test
