e9ae98b31dd84e3f11edd487889d10f5e11754d0
Theron Wang <doritocat2015@gmail.com>
2024-07-01 21:22:24 -0400

    test case is run WITHOUT jar file

--- src/test/resources/annoingenerictarget/expected/org/checkerframework/checker/nullness/qual/NonNull.java
+++ src/test/resources/annoingenerictarget/expected/org/checkerframework/checker/nullness/qual/NonNull.java
@@ -1,21 +1,4 @@
 package org.checkerframework.checker.nullness.qual;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-import org.checkerframework.framework.qual.DefaultFor;
-import org.checkerframework.framework.qual.DefaultQualifierInHierarchy;
-import org.checkerframework.framework.qual.QualifierForLiterals;
-import org.checkerframework.framework.qual.SubtypeOf;
-import org.checkerframework.framework.qual.UpperBoundFor;
-
-@Documented
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ ElementType.TYPE_USE, ElementType.TYPE_PARAMETER })
-@SubtypeOf({ MonotonicNonNull.class })
-@DefaultQualifierInHierarchy
-@QualifierForLiterals({ LiteralKind.STRING })
-@DefaultFor({ TypeUseLocation.EXCEPTION_PARAMETER })
-@UpperBoundFor(typeKinds = { TypeKind.PACKAGE, TypeKind.INT, TypeKind.BOOLEAN, TypeKind.CHAR, TypeKind.DOUBLE, TypeKind.FLOAT, TypeKind.LONG, TypeKind.SHORT, TypeKind.BYTE })
 public @interface NonNull {
 }
