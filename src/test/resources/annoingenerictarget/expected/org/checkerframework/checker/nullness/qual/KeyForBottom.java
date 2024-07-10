25d3d573f6f3aad84d8477b064535aad3dbb4dbc
Theron Wang <doritocat2015@gmail.com>
2024-07-09 21:43:52 -0400

    Adjust test cases

--- src/test/resources/annoingenerictarget/expected/org/checkerframework/checker/nullness/qual/KeyForBottom.java
+++ src/test/resources/annoingenerictarget/expected/org/checkerframework/checker/nullness/qual/KeyForBottom.java
@@ -1,5 +1,5 @@
 package org.checkerframework.checker.nullness.qual;
 
-@java.lang.annotation.Target(java.lang.annotation.ElementType.TYPE_USE)
+@java.lang.annotation.Target({ java.lang.annotation.ElementType.TYPE_USE, java.lang.annotation.ElementType.METHOD })
 public @interface KeyForBottom {
 }
--- src/test/resources/syntheticannotations/expected/com/example/Baz.java
+++ src/test/resources/syntheticannotations/expected/com/example/Baz.java
@@ -1,6 +1,6 @@
 package com.example;
 
-@java.lang.annotation.Target(java.lang.annotation.ElementType.TYPE_USE)
+@java.lang.annotation.Target({ java.lang.annotation.ElementType.TYPE_USE, java.lang.annotation.ElementType.METHOD })
 public @interface Baz {
 
     public String foo();
