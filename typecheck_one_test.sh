a8a234e37816fd3d9959ec322e7e7cd95fb899cd
Martin Kellogg <martin.kellogg@njit.edu>
2024-07-25 17:12:01 -0400

    qol improvement to run just one test

---
+++ typecheck_one_test.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+## This script runs javac on a single expected test output. If the test output
+## is compilable, it exits with code 0. If not, it exits with code 1. In all other
+# cases (e.g., if a cd command fails), it exits with code 2. It includes
+## logic for skipping test cases that don't need to compile, etc. It should be
+## run in the directory src/test/resources.
+
+testcase=$1
+
+if [ "${testcase}" = "shared" ]; then exit 0; fi
+# https://bugs.openjdk.org/browse/JDK-8319461 wasn't actually fixed (this test is based on that bug)
+if [ "${testcase}" = "superinterfaceextends" ]; then exit 0; fi
+# incomplete handling of method references: https://github.com/njit-jerse/specimin/issues/291
+# this test exists to check that no crash occurs, not that Specimin produces the correct output
+if [ "${testcase}" = "methodref2" ]; then exit 0; fi
+cd "${testcase}/expected/" || exit 2
+# javac relies on word splitting
+# shellcheck disable=SC2046
+javac -classpath "../../shared/checker-qual-3.42.0.jar" $(find . -name "*.java") \
+  || { echo "Running javac on ${testcase}/expected issues one or more errors, which are printed above."; \
+  find . -name "*.class" -exec rm {} \; ; exit 1; }
+
+# clean up
+find . -name "*.class" -exec rm {} \;
+
+cd ../.. || exit 2
+exit 0
--- typecheck_test_outputs.sh
+++ typecheck_test_outputs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # This script runs javac on all of the expected test outputs under src/test/resources.
-# It returns 2 if any of them fail to compile, 1 if there are any malformed test directories,
+# It returns 1 if any of them fail to compile, 2 if there are any malformed test directories,
 # and 0 if all of them do compile.
 #
 # It is desirable that all of the expected test outputs compile, because Specimin
@@ -11,26 +11,18 @@ returnval=0
 
 cd src/test/resources || exit 1
 for testcase in * ; do
-    if [ "${testcase}" = "shared" ]; then continue; fi
-    # https://bugs.openjdk.org/browse/JDK-8319461 wasn't actually fixed (this test is based on that bug)
-    if [ "${testcase}" = "superinterfaceextends" ]; then continue; fi
-    # incomplete handling of method references: https://github.com/njit-jerse/specimin/issues/291
-    # this test exists to check that no crash occurs, not that Specimin produces the correct output
-    if [ "${testcase}" = "methodref2" ]; then continue; fi
-    cd "${testcase}/expected/" || exit 1
-    # javac relies on word splitting
-    # shellcheck disable=SC2046
-    javac -classpath "../../shared/checker-qual-3.42.0.jar" $(find . -name "*.java") \
-      || { echo "Running javac on ${testcase}/expected issues one or more errors, which are printed above."; returnval=2; }
-    cd ../.. || exit 1
+    sh ../../../typecheck_one_test.sh "${testcase}"
+    # update overall return value
+    test_retval=$?
+    if [ ! "${test_retval}" = 0 ]; then
+      returnval=1
+    fi
 done
 
 if [ "${returnval}" = 0 ]; then
   echo "All expected test outputs compiled successfully."
-elif [ "${returnval}" = 2 ]; then
+elif [ "${returnval}" = 1 ]; then
   echo "Some expected test outputs do not compile successfully. See the above error output for details."
 fi
-
-find . -name "*.class" -exec rm {} \;
 
 exit ${returnval}
