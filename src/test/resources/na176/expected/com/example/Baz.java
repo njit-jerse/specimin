3195307e9aba951dc5105632d0623b93b3f96bb6
Martin Kellogg <martin.kellogg@njit.edu>
2024-07-26 12:15:43 -0400

    maybe the right expected outputs

---
+++ src/test/resources/na176/expected/com/example/Baz.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Baz<T> {
+
+    public static TestReturnType test() {
+        throw new Error();
+    }
+}
---
+++ src/test/resources/na176/input/com/example/Foo.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Foo {
+    public void foo() {
+        Baz.test();
+    }
+
+    public void bar() {
+        Baz<String> baz;
+    }
+}
