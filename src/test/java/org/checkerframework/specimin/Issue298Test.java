528d3407ff96ea9972140b432b62eb31f83466ad
Theron Wang <doritocat2015@gmail.com>
2024-07-26 13:59:09 -0400

    re-add support for fields

--- src/test/java/org/checkerframework/specimin/Issue298Test.java
+++ src/test/java/org/checkerframework/specimin/Issue298Test.java
@@ -7,7 +7,7 @@ import org.junit.Test;
  * This test is a simplified version of the problem described by <a
  * href="https://github.com/njit-jerse/specimin/issues/298">...</a>. The test is somewhat limited -
  * it triggers part of the problem, but not all of it: in particular, the small test case doesn't
- * trigger the "resolved yet stuck method call" code in {@link TargetMethodFinderVisitor}, which is
+ * trigger the "resolved yet stuck method call" code in {@link TargetMemberFinderVisitor}, which is
  * part of the real bug.
  */
 public class Issue298Test {
