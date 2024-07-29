1902e90631a73c73236757615003d59b4349ef15
Martin Kellogg <martin.kellogg@njit.edu>
2024-07-29 13:30:17 -0400

    test that actually works

--- src/test/resources/whenthen/expected/org/example/TestUtil.java
+++ src/test/resources/whenthen/expected/org/example/TestUtil.java
@@ -2,15 +2,15 @@ package org.example;
 
 public class TestUtil {
 
+    public static <SyntheticUnconstrainedType> SyntheticUnconstrainedType mock(int parameter0) {
+        throw new Error();
+    }
+
     public static OrgExampleTestUtilWhenReturnType when(int parameter0) {
         throw new Error();
     }
 
     public static OrgExampleTestUtilWhenReturnType when(java.lang.String parameter0) {
-        throw new Error();
-    }
-
-    public static <SyntheticUnconstrainedType> SyntheticUnconstrainedType mock(int parameter0) {
         throw new Error();
     }
 }
--- src/test/resources/whenthen/input/com/example/Simple.java
+++ src/test/resources/whenthen/input/com/example/Simple.java
@@ -3,9 +3,13 @@ package com.example;
 import static org.example.TestUtil.when;
 import static org.example.TestUtil.mock;
 
+import com.example.Banana;
+
 class Simple {
-    // Target method.
+
     void bar() {
+        Integer s = mock(2);
+        Banana b = mock(3);
         when(5).then("Foo");
         when("bar").then(mock(1));
     }
