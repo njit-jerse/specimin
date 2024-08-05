f6a79148261dfb5717f1bd168cce321fd557219b
Theron Wang <doritocat2015@gmail.com>
2024-08-05 12:13:58 -0400

    temp commit to see if CI fails

--- src/test/resources/abstractimpl/expected/com/example/Simple.java
+++ src/test/resources/abstractimpl/expected/com/example/Simple.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 
 import com.example.WrappedSet;
 
-public class Simple<K, V> {
+public class Simply<K, V> {
     Collection<V> bar(K key, Collection<V> collection) {
         return new WrappedSet(key, (Set<V>) collection);
     }
