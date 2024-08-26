20321ff56b055a7880ad64a942f24022248d036d
Theron Wang <doritocat2015@gmail.com>
2024-08-26 12:22:18 -0400

    remove unused imports

--- src/test/resources/preserveannotations/expected/org/checkerframework/checker/index/qual/GTENegativeOne.java
+++ src/test/resources/preserveannotations/expected/org/checkerframework/checker/index/qual/GTENegativeOne.java
@@ -1,7 +1,6 @@
 package org.checkerframework.checker.index.qual;
 
 import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
