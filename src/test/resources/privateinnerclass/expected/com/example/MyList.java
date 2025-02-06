3bca0fe67d84eea4708814da4f7bd1b8242af7bd
Martin Kellogg <martin.kellogg@njit.edu>
2025-02-06 09:52:22 -0500

    test case

---
+++ src/test/resources/privateinnerclass/expected/com/example/MyList.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class MyList<T> {
+
+    public MyList() {
+        throw new java.lang.Error();
+    }
+
+    public AddReturnType add(T parameter0) {
+        throw new java.lang.Error();
+    }
+}
