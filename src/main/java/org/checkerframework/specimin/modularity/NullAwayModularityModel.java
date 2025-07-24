224e42791244239ade5527f2e7e8954301ff3a7d
Theron Wang <tw.theron.wang@gmail.com>
2025-07-24 14:34:54 -0400

    add deleted files

--- src/main/java/org/checkerframework/specimin/modularity/NullAwayModularityModel.java
+++
@@ -1,9 +0,0 @@
-package org.checkerframework.specimin.modularity;
-
-/** The modularity model for NullAway. */
-public class NullAwayModularityModel implements ModularityModel {
-  @Override
-  public boolean preserveAllFieldsIfTargetIsConstructor() {
-    return true;
-  }
-}
