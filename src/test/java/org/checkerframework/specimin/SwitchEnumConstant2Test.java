40e895d906e1c59f2fa1266cf2418d0ed624e8b2
Martin Kellogg <martin.kellogg@njit.edu>
2026-07-06 12:23:08 -0400

    adapt to updated junit

--- src/test/java/org/checkerframework/specimin/SwitchEnumConstant2Test.java
+++ src/test/java/org/checkerframework/specimin/SwitchEnumConstant2Test.java
@@ -1,7 +1,7 @@
 package org.checkerframework.specimin;
 
 import java.io.IOException;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * This test checks that Specimin properly handles the scoping of enum constants used in switch
