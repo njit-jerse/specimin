978ede64238b8599003e8fe20ffcc387550af3b2
Martin Kellogg <martin.kellogg@njit.edu>
2026-07-28 15:06:17 -0400

    adjust names and javadoc

--- src/test/java/org/checkerframework/specimin/StaticImportReferenceTest.java
+++ src/test/java/org/checkerframework/specimin/StaticImportReferenceTest.java
@@ -3,12 +3,15 @@ package org.checkerframework.specimin;
 import java.io.IOException;
 import org.junit.jupiter.api.Test;
 
-/** This test checks if Specimin can handle an unsolved field two layers deep in a static import. */
-public class StaticImportField2Test {
+/**
+ * This test checks if Specimin can handle an unsolved imported static field that itself has an
+ * unsolved imported field of another fixed type.
+ */
+public class StaticImportReferenceTest {
   @Test
   public void runTest() throws IOException {
     SpeciminTestExecutor.runTestWithoutJarPaths(
-        "staticimportfield2",
+        "staticimportreference",
         new String[] {"com/example/Simple.java"},
         new String[] {"com.example.Simple#test()"});
   }
