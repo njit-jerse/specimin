16e0226f59c42645c524ce71a34be2a6e4825a8c
Martin Kellogg <martin.kellogg@njit.edu>
2026-07-28 08:51:32 -0400

    fix the test

--- src/test/java/org/checkerframework/specimin/WrappedNodeTest.java
+++ src/test/java/org/checkerframework/specimin/WrappedNodeTest.java
@@ -1,17 +1,14 @@
 package org.checkerframework.specimin;
 
 import java.io.IOException;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-/**
- * This test checks that a simple Java file with no dependencies and a single target method with one
- * method that it depends on results in that depended-on method being replaced by an empty body.
- */
-public class wrappedNode {
+/** TODO. */
+public class WrappedNodeTest {
   @Test
   public void runTest() throws IOException {
     SpeciminTestExecutor.runTestWithoutJarPaths(
-        "wrappedNode",
+        "wrappednode",
         new String[] {"com/example/MappingIterator.java"},
         new String[] {"com.example.MappingIterator#readAll()"});
   }
