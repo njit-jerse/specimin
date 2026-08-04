9be924f2775b31ee264c4ad283d2a6c40a6d2fbb
Martin Kellogg <martin.kellogg@njit.edu>
2026-08-04 12:28:40 -0400

    use fqns

--- src/test/resources/issue403/expected/com/fasterxml/jackson/annotation/ObjectArrayDeserializer.java
+++ src/test/resources/issue403/expected/com/fasterxml/jackson/annotation/ObjectArrayDeserializer.java
@@ -1,5 +1,5 @@
 package com.fasterxml.jackson.annotation;
-public class ObjectArrayDeserializer extends com.fasterxml.jackson.annotation.ValueDeserializer<Object> {
+public class ObjectArrayDeserializer extends com.fasterxml.jackson.annotation.ValueDeserializer<java.lang.Object> {
 
     public ObjectArrayDeserializer(com.fasterxml.jackson.annotation.ArrayType parameter0, com.fasterxml.jackson.annotation.ValueDeserializer<java.lang.Object> parameter1, tools.jackson.databind.jsontype.TypeDeserializer parameter2) {
         throw new java.lang.Error();
