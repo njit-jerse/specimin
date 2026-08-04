28fd60c73ac44b9f04c4751796c26cc178a1fc4c
Martin Kellogg <martin.kellogg@njit.edu>
2026-08-04 12:29:19 -0400

    test case

---
+++ src/test/resources/wildcardsupertype/expected/com/example/Simple.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.example.Bar;
+import org.example.Baz;
+import org.example.Qux;
+
+public class Simple {
+
+  public void target(Bar b) {
+    Baz<?> x = b.get();
+    x = new Qux();
+    System.out.println(x);
+  }
+}
