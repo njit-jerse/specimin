c4db0b5c28c5b5afe266cefcefa12af177c7023b
Martin Kellogg <martin.kellogg@njit.edu>
2026-08-07 16:50:38 -0400

    simplify comment

--- src/test/java/org/checkerframework/specimin/SyntheticEnumWithConstructorTest.java
+++ src/test/java/org/checkerframework/specimin/SyntheticEnumWithConstructorTest.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 import org.junit.jupiter.api.Test;
 
 /**
- * This test checks what Specimin does when a synthetic type is both inferred to be an enum (because
- * one of its fields is used as an annotation argument) and given a constructor (because it is
- * instantiated). An enum constructor may not be public, so a naive rendering does not compile.
+ * This test checks what Specimin does when a synthetic type is has one of its fields is used as an
+ * annotation argument and is instantiated. An enum constructor may not be public, so if it created
+ * as an enum then it would not compile.
  */
 public class SyntheticEnumWithConstructorTest {
   @Test
