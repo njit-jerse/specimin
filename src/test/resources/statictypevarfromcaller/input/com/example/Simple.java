441c0156f6708f01be00ae2db2318ef61f475516
Martin Kellogg <martin.kellogg@njit.edu>
2026-08-12 16:52:17 -0400

    test case

---
+++ src/test/resources/statictypevarfromcaller/input/com/example/Simple.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import com.example.other.Box;
+import com.example.other.Src;
+
+public class Simple {
+  public static <T> Box<T> target(Src<T> src) {
+    return Box.from(src);
+  }
+}
