7a2a935780012911d96a0a4eed24ed092eb835fc
Martin Kellogg <martin.kellogg@njit.edu>
2026-08-20 08:48:13 -0400

    A few more tests for the interaction between `sealed` and #513 (#515)
    These were uncommitted in my local repo and should have been added with
    the changes in response to code rabbit's review before I merged #513.

---
+++ src/test/resources/sealedpermitsgeneratedsubtype/expected/com/example/SealedBase.java
@@ -0,0 +1,3 @@
+package com.example;
+
+public sealed class SealedBase permits Baz {}
---
+++ src/test/resources/sealedpermitsgeneratedsubtype/input/com/example/Simple.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.example.Foo;
+
+public class Simple {
+  public void target(Foo f) {
+    Baz b = (Baz) f.get();
+    SealedBase s = f.get();
+    System.out.println(b + "" + s);
+  }
+}
