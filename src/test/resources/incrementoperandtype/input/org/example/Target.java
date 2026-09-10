d4caac4657f7bfd9ef9f27e938f78c832260eed6
Martin Kellogg <martin.kellogg@njit.edu>
2026-09-10 09:56:49 -0400

    Test that I forgot to `git add` before I pushed #551 (#552)
    #551 should have contained this test case, which I wrote in response to
    the CodeRabbit comments but forgot to `git add`.

---
+++ src/test/resources/incrementoperandtype/input/org/example/Target.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import external.Metrics;
+
+public class Target {
+  public char charPrefix(Metrics metrics) {
+    char result = ++metrics.code;
+    return result;
+  }
+
+  public short shortPostfix(Metrics metrics) {
+    short result = metrics.small++;
+    return result;
+  }
+
+  public char charReturn(Metrics metrics) {
+    return ++metrics.other;
+  }
+
+  public Object boxed(Metrics metrics) {
+    Object result = ++metrics.opaque;
+    return result;
+  }
+}
